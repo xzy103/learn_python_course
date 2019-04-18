fe8e8914918f8edeb6fd0fa9152625c4798a85ac
WTG <1342638994@qq.com>
2019-04-18 09:39:03 +0800

    upgrade test

--- chap02/2.7.py
+++ chap02/2.7.py
@@ -19,11 +19,23 @@ def get_normol(mean, dev):
 
 
 random.seed()
-cost = 50000  # 固定成本
-sale_mean, sale_dev = 100000, 25000
-cost_mean, cost_dev = 5, 1
-price_mean, price_dev = 10, 2
-rate_mean, rate_dev = 0.1, 0.02
+stable_cost = 50000  # 固定成本
+sale_mean, sale_dev = 100000, 25000  # 销量的均值 方差
+cost_mean, cost_dev = 5, 1  # 单位成本 均值 方差
+price_mean, price_dev = 10, 2  # 价格 均值 方差
+rate_mean, rate_dev = 0.1, 0.02  # 利率 均值 方差
 
+n = 100000
+count = 0
+profits = 0
+for i in range(n):
+    sale = get_normol(sale_mean, sale_dev)
+    price = get_normol(price_mean, price_dev)
+    cost = get_normol(cost_mean, cost_dev)
+    rate = get_normol(rate_mean, rate_dev)
+    profit = sale*((price/(1+rate))-cost)-stable_cost
+    if profit > 0:
+        count += 1
+        profits += profit
 
-
+print(f"该项目盈利的可能性为{count/n*100:.2f}%，盈利的期望值为{profits/n:.2f}元。")
--- chap02/2.8.py
+++ chap02/2.8.py
@@ -1,3 +1,50 @@
 # -*- coding:UTF-8 -*-
 # Author:徐熙林-金融162班
-# Date:
+# Date:2019.4.18
+
+import random
+
+
+def rend(t):
+    random.seed()
+    n = 10000
+    global customer_a, customer_b, no_bike_count_a, no_bike_count_b, ratio_a, ratio_b
+    prob_a, prob_b = 0.3, 0.5  # A地租车可能性 B地租车可能性
+    num_a, num_b = 100, 100  # AB两地车的数量
+    customer_a, customer_b = 0, 0  # AB两地来租车的人数
+    no_bike_count_a, no_bike_count_b = 0, 0  # AB两地没租到车的人数
+    for i in range(n):
+        want_a = random.uniform(0, 1) <= prob_a
+        want_b = random.uniform(0, 1) <= prob_b
+        if want_a:
+            customer_a += 1
+            if num_a > 0:
+                num_a -= 1
+                num_b += 1
+            else:
+                no_bike_count_a += 1
+        if want_b:
+            customer_b += 1
+            if num_b > 0:
+                num_b -= 1
+                num_a += 1
+            else:
+                no_bike_count_b += 1
+
+        if i % t == 0:
+            trans = num_a//2
+            num_a -= trans
+            num_b += trans
+
+    ratio_a = no_bike_count_a / customer_a * 100
+    ratio_b = no_bike_count_b / customer_b * 100
+    return ratio_b
+
+
+t = 1
+while rend(t) < 5:
+    t += 1
+
+print(f"每隔{t}小时转移一次为宜。")
+print(f"A地想租车人数{customer_a},未租到人数{no_bike_count_a}, 未租到比例{ratio_a:0.2f}%")
+print(f"B地想租车人数{customer_b},未租到人数{no_bike_count_b}, 未租到比例{ratio_b:0.2f}%")
