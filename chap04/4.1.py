7a306d23619db5be24fafa233d18e76d0f98f767
xzy103 <1342638994@qq.com>
2019-05-25 16:13:04 +0800

    修改细节

--- chap04/4.1.py
+++ chap04/4.1.py
@@ -5,7 +5,7 @@
 
 def factor_sum(n: int):
     """
-    找出一个正整数所有因子的和
+    计算一个正整数所有因子的和
     :param n: 一个正整数
     :return: 这个正整数所有因子的和
     """
@@ -18,9 +18,11 @@ def factor_sum(n: int):
 
 tip = """提示：(a,b) -> (220,284) 和 (284,220)视为相同的一对\n>>>>>Begin..."""
 print(tip)
+
 n = 20000
 for i in range(1, n+1):
     result = factor_sum(i)
     if factor_sum(result) == i <= result:
         print('Bingo!Find a pair:', i, result)
+
 print('Done!')
