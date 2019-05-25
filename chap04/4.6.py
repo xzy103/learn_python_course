ba3ed1b94df222bd9b9667533ec4629c0edb69b5
xzy103 <1342638994@qq.com>
2019-05-25 16:22:25 +0800

    添加注释 优化细节

--- chap04/4.6.py
+++ chap04/4.6.py
@@ -7,15 +7,15 @@ import csv
 
 
 class UserInfo:
-    def __init__(self, id, username, name, gender, Tel, password, mail, rank):
-        self.id = id
-        self.username = username
-        self.name = name
+    def __init__(self, id_, username, name, gender, Tel, password, mail, rank):
+        self.id = id_
+        self.username = username  # 用户名
+        self.name = name  # 用户姓名
         self.gender = gender
         self.Tel = Tel
         self.password = password
         self.mail = mail
-        self.rank = rank
+        self.rank = rank  # 用户等级
 
 
 def read_csv(filename: str):
@@ -24,8 +24,8 @@ def read_csv(filename: str):
         next(userls)
         users = []
         for u in userls:
-            id, username, name, gender, Tel, password, mail, rank = u[0], u[1], u[2], u[3], u[4], u[5], u[6], u[7]
-            user = UserInfo(id, username, name, gender, Tel, password, mail, rank)
+            id_, username, name, gender, Tel, password, mail, rank = u[0], u[1], u[2], u[3], u[4], u[5], u[6], u[7]
+            user = UserInfo(id_, username, name, gender, Tel, password, mail, rank)
             users.append(user)
     return users
 
@@ -38,7 +38,7 @@ def filter_vip(obj):
     return 'VIP' in obj.rank
 
 
-users = read_csv(filename='../chap03/ex2-1.用户信息.csv')
+users = read_csv(filename='../chap03/ex2-1.用户信息.csv')  # 相对路径
 hotmails = list(filter(filter_mail, users))
 vips = list(filter(filter_vip, hotmails))
 print(f'使用hotmail邮箱的用户个数为{len(hotmails)}，其中vip用户个数为{len(vips)}')
