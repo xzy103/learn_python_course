c042a9d74899f72035d82afa1986182fcac76eb8
xzy103 <1342638994@qq.com>
2019-05-25 11:31:06 +0800

    修改细节

--- chap02/2.3.py
+++ chap02/2.3.py
@@ -3,10 +3,10 @@
 # Date:2019.3.21
 
 p = float(input("请输入本金："))
-r = float(input("请输入利率（小数）："))
+r = float(input("请输入利率(小数)："))
 n = int(input("请输入存款年份："))
 
 for i in range(1, n+1):
     interest = p*r  # 当年新增利息
     p += interest  # 当年年末账户余额
-    print("第{:^2}年的新增利息为{:<8} 账户总金额为{:<8}".format(i, round(interest, 2), round(p, 2)))
+    print(f"第{i:^3}年的新增利息为{interest:<8.2f} 账户总金额为{p:<8.2f}")
