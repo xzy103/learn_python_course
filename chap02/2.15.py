0bc21e39503e8e6f1cff09182306d7eae8e50dc9
xzy103 <1342638994@qq.com>
2019-05-25 13:21:50 +0800

    修改细节 更新注释

--- chap02/2.15.py
+++ chap02/2.15.py
@@ -3,22 +3,23 @@
 # Date:2019.4.23
 
 
-"""
+s = """
 2.15分解质因数方法简介
 获取一个正整数
 遍历小于这个数的每一个正整数，判断是否为其因数
 找到一个因数后更新这一整数为 整数除以因数(while loop)
 """
 
+print(s)
 n = int(input('请输入一个正整数：'))
 m = n  # 找到一个因数i后，m /= i，m的初值设为0
 a = 0  # 用来暂存因数
 
-for i in range(2, n):
+for i in range(2, n):  # 遍历小于这一整数的正整数
     for j in range(2, m):
         if m % i == 0 and i != a:
-            while m % i == 0:
-                m = int(m/i)  # while用来过滤重复的因数
+            while m % i == 0:  # while用来过滤重复的因数
+                m = int(m/i)
             a = i
             print(a, end=' ')
 
