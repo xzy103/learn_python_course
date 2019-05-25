cbc94d29a52dbb057502e59cf6c6c1289d5114df
xzy103 <1342638994@qq.com>
2019-05-25 15:18:15 +0800

    修改细节 添加注释

--- chap03/3.4.2.py
+++ chap03/3.4.2.py
@@ -7,22 +7,24 @@ import csv
 
 
 class Contacts:
-    def __init__(self, id, name, Tel, mail, unit):
-        self.id = id
+    def __init__(self, id_, name, Tel, mail, unit):
+        self.id = id_
         self.name = name
         self.Tel = Tel
         self.mail = mail
-        self.unit = unit
+        self.unit = unit  # 工作单位
 
 
-with open('ex2-3.contacts.csv', 'r', encoding='utf-8') as fr:
+filename = 'ex2-3.contacts.csv'
+with open(filename, 'r', encoding='utf-8') as fr:
     contactsls = csv.reader(fr)
     next(contactsls)
     contacts = []
     for s in contactsls:
-        id, name, Tel, mail, unit = s[0], s[1], s[2], s[3], s[4]
-        contact = Contacts(id, name, Tel, mail, unit)
+        id_, name, Tel, mail, unit = s[0], s[1], s[2], s[3], s[4]
+        contact = Contacts(id_, name, Tel, mail, unit)
         contacts.append(contact)
+
 
 print('id,姓名,电话,邮箱,单位')
 print('以下是所有上海易利生物科技有限公司的联系人')
@@ -31,11 +33,13 @@ for s in contacts:
         print(f'{s.id},{s.name},{s.Tel},{s.mail},{s.unit}')
 print()
 
+
 print('以下是所有使用雅虎邮箱的联系人')
 for s in contacts:
-    if '@yahoo.com' in s.mail:
+    if '@yahoo.com' in s.mail:  # 如果邮箱中含有指定字段
         print(f'{s.id},{s.name},{s.Tel},{s.mail},{s.unit}')
 print()
+
 
 # 移动号段 134,135,136,137,138,139,147,150,151,152,157,158,159,182,183,187,188
 print('以下是所有使用中国移动手机号的联系人')
