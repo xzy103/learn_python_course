3e168ec7fc24fc1d7e415471ddd064e3cbdc77fc
xzy103 <1342638994@qq.com>
2019-05-25 13:24:55 +0800

    修改细节 更新注释

--- chap02/2.16.py
+++ chap02/2.16.py
@@ -3,30 +3,24 @@
 # Date:2019.4.26
 
 
-"""
+s = """
 2.16分解质因数方法简介
 获取一个正整数
 找到小于这个数的所有质数
 在质数中寻找质因数
 """
 
-
+print(s)
 n = int(input('请输入一个正整数：'))
 ls = []
+
 for i in range(2, n//2+1):
     is_prime = True
     for j in range(2, i//2+1):
         if i % j == 0:
             is_prime = False
             break  # 如果break，则i不是质数
-    if is_prime and n % i == 0:
-        while n % i == 0:
+    if is_prime and n % i == 0:  # 如果是质数且是因数
+        while n % i == 0:  # while用来过滤重复的因数
             n = int(n/i)
         print(i, end=' ')
-
-
-
-
-
-
-
