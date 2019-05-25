2e5f6c26cfc8c748217fd982af27918e1431bb70
xzy103 <1342638994@qq.com>
2019-05-25 15:08:40 +0800

    修改细节 添加注释

--- chap03/3.3.2.py
+++ chap03/3.3.2.py
@@ -15,7 +15,8 @@ class Score:
         self.literature = literature
 
 
-with open('ex2-2.scores.csv', 'r', encoding='utf-8') as fr:
+filename = 'ex2-2.scores.csv'
+with open(filename, 'r', encoding='utf-8') as fr:
     scorels = csv.reader(fr)
     next(scorels)
     scores = []
@@ -26,11 +27,12 @@ with open('ex2-2.scores.csv', 'r', encoding='utf-8') as fr:
 
 info1_sum_math = info1_sum_literture = count = 0
 for score in scores:
-    if score.class_ == '信管1班':
+    if score.class_ == '信管1班':  # 仅针对满足条件的对象操作
         count += 1
         info1_sum_math += float(score.math)
         info1_sum_literture += float(score.literature)
 
-info1_avg_math = info1_sum_math/count
-info1_avg_literture = info1_sum_literture/count
-print(f'信管1班的数学平均成绩是{round(info1_avg_math, 2)}，语文平均成绩是{round(info1_avg_literture, 2)}。')
+info1_avg_math = info1_sum_math/count  # 信管1班数学平均成绩
+info1_avg_literture = info1_sum_literture/count  # 信管1班语文平均成绩
+
+print(f'信管1班数学平均成绩是{info1_avg_math:.2f}，语文平均成绩是{info1_avg_literture:.2f}。')
