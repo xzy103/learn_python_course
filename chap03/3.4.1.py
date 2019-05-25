7a574c3ac826cbfb571c54fef18086f6c16f5eb1
xzy103 <1342638994@qq.com>
2019-05-25 15:13:45 +0800

    修改细节 添加注释

--- chap03/3.4.1.py
+++ chap03/3.4.1.py
@@ -7,21 +7,22 @@ import csv
 
 
 class Contacts:
-    def __init__(self, id, name, Tel, mail, unit):
-        self.id = id
+    def __init__(self, id_, name, Tel, mail, unit):
+        self.id = id_
         self.name = name
         self.Tel = Tel
         self.mail = mail
-        self.unit = unit
+        self.unit = unit  # 工作单位
 
 
-with open('ex2-3.contacts.csv', 'r', encoding='utf-8') as fr:
+filename = 'ex2-3.contacts.csv'
+with open(filename, 'r', encoding='utf-8') as fr:
     contactsls = csv.reader(fr)
     next(contactsls)
     contacts = []
     for s in contactsls:
-        id, name, Tel, mail, unit = s[0], s[1], s[2], s[3], s[4]
-        contact = Contacts(id, name, Tel, mail, unit)
+        id_, name, Tel, mail, unit = s[0], s[1], s[2], s[3], s[4]
+        contact = Contacts(id_, name, Tel, mail, unit)
         contacts.append(contact)
 
 print('id,姓名,电话,邮箱,单位')
