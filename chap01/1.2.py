50d6505eb622a0e3936884aa032280a345bdde23
xzy103 <1342638994@qq.com>
2019-05-25 10:29:23 +0800

    修改细节

--- chap01/1.2.py
+++ chap01/1.2.py
@@ -3,7 +3,7 @@
 # Date:2019.3.21
 
 p = float(input("请输入本金："))
-r = float(input("请输入利率（小数）："))
+r = float(input("请输入利率(小数)："))
 n = int(input("请输入存款年份："))
 total = round(p*(1+r)**n, 2)
 print("到期总金额为：", total)
