b1a8c61a2025b318e18c4bf0a68446cbc40ee5be
xzy103 <1342638994@qq.com>
2019-05-25 15:25:09 +0800

    修改细节 添加注释

--- chap03/3.5.1.py
+++ chap03/3.5.1.py
@@ -7,20 +7,21 @@ import csv
 
 
 class Inventory:
-    def __init__(self, id, name, amount, type_):
-        self.id = id
+    def __init__(self, id_, name, amount, type_):
+        self.id = id_
         self.name = name
         self.amount = amount
-        self.type = type_
+        self.type = type_  # 商品类型
 
 
-with open('ex2-4.inventory.csv', 'r', encoding='utf-8') as fr:
+filename = 'ex2-4.inventory.csv'
+with open(filename, 'r', encoding='utf-8') as fr:
     inventoryls = csv.reader(fr)
     next(inventoryls)
     inventory = []
     for i in inventoryls:
-        id, name, amount, type_ = i[0], i[1], i[2], i[3]
-        inv = Inventory(id, name, amount, type_)
+        id_, name, amount, type_ = i[0], i[1], i[2], i[3]
+        inv = Inventory(id_, name, amount, type_)
         inventory.append(inv)
 
 print('商品ID,商品名称,实际库存量,商品类别')
