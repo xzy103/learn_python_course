455952a6ae5da12d97a44b9bdddae4bbbfba07b6
xzy103 <1342638994@qq.com>
2019-05-25 15:04:36 +0800

    修改细节 添加注释

--- chap03/3.3.1.py
+++ chap03/3.3.1.py
@@ -15,7 +15,8 @@ class Score:
         self.literature = literature
 
 
-with open('ex2-2.scores.csv', 'r', encoding='utf-8') as fr:
+filename = 'ex2-2.scores.csv'
+with open(filename, 'r', encoding='utf-8') as fr:
     scorels = csv.reader(fr)
     next(scorels)
     scores = []
@@ -24,12 +25,12 @@ with open('ex2-2.scores.csv', 'r', encoding='utf-8') as fr:
         score = Score(id_, name, class_, math, literature)
         scores.append(score)
 
-sum_math = sum_literature = 0
+sum_math = sum_literature = 0  # 初始化数学和语文成绩的和
 for score in scores:
     sum_math += float(score.math)
     sum_literature += float(score.literature)
-avg_math = sum_math/len(scores)
-avg_literature = sum_literature/len(scores)
-print(f'所有同学的数学平均成绩是{round(avg_math, 2)}，语文平均成绩是{round(avg_literature, 2)}。')
+avg_math = sum_math/len(scores)  # 数学平均成绩
+avg_literature = sum_literature/len(scores)  # 语文平均成绩
+print(f'所有同学的数学平均成绩是{avg_math:.2f}，语文平均成绩是{avg_literature:.2f}。')
 
 dialog.show_objects(scores, title='成绩信息', width=1080, height=720)
